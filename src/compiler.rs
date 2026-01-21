8ac1939e36a3987d20712146969651faa40d1b9f
AlexPham0209 <AlexLPham0209@gmail.com>
2026-01-21 01:15:05 -0500

    Started working on the bytecode instructions

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,19 +1,38 @@
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+
+
+#[derive(FromPrimitive)]
 enum Opcode {
-    Return,
+    Return = 1,
 }
 
-struct Chunk {
-    count: usize,
-    capacity: usize,
-    code: Vec<u32>,
+
+pub struct Chunk {
+    pub code: Vec::<u8>
 }
 
 impl Chunk {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Chunk {
-            count: 0,
-            capacity: 0,
-            code: Vec::<u32>::new(),
+            code: Vec::<u8>::new()
         }
+    }
+
+    pub fn write(&mut self, byte: u8) {
+        self.code.push(byte);
+    }
+
+    pub fn disassemble(&self) -> String {
+        self.code.iter().copied().map(Chunk::disassemble_instruction).collect::<Vec<String>>().join("\n")
+    }
+
+    fn disassemble_instruction(byte: u8) -> String {
+        let instr = match FromPrimitive::from_u8(byte) {
+            Some(Opcode::Return) => "Return",
+            None => "Unknown Instruction"
+        };
+
+        instr.to_string()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ mod pho;
 mod vm;
 
 fn main() {
-    println!("Hello, world!");
     let lexer = Lexer::read("bin\\test.txt");
     for c in &lexer.tokens {
         println!("{:?}", c);
